6051f5eb411f56439700cc6301029ee51c838b73
paulina koch <pjassakoch@gmail.com>
2024-06-07 11:43:01 -0400

    Reorganized file structure

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+// import './App.css'
+import Info from './components/Info';
+
+
+
+function App() {
+
+
+  return (
+    <div>
+      <Info/>
+      {/* <About/>
+      <Interest/>
+      <Footer/> */}
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Info() {
+    return (
+        <div>
+            <img src="/images/avataaars.png" alt="headshot"/>
+            <h1>why is nothing showing up</h1>
+            <p>helllooo</p>
+
+        </div>
+    );
+}
