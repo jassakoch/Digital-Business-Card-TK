804bbb25dc26b8bab2a6210cbeac1a694d6a7f06
paulina koch <pjassakoch@gmail.com>
2024-06-09 17:25:49 -0400

    Fixed layout issues in Info componenet and added google fonts

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 export default function Info() {
     return (
-        <div>
-            <img src="/images/avataaars.png" alt="headshot"/>
-            <h1>why is nothing showing up</h1>
-            <p>helllooo</p>
+        <div className="info-container">
+            <div className="headshot-container">
+                <img src="/images/avataaars.png" alt="headshot" className="headshot" />
+            </div>
+            <div className="info">
+            <h1 className="name">Tim Koch</h1>
+            <h2 className="job-title"> Shotcrete Specialists</h2>
+            <h3 className="website">Tim Koch Construction Ltd website</h3>
+            <button>Email</button>
+            </div>
 
         </div>
     );
